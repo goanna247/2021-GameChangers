201ad3f685923d6ec4fd3693ccb6f6211a0e1b90
Anna <anna.pedersen@student.education.wa.edu.au>
2021-01-23 16:25:15 +0800

    added falcon stuff

--- 4788/src/main/cpp/strategies/FalconStrat.cpp
+++ 4788/src/main/cpp/strategies/FalconStrat.cpp
@@ -4,7 +4,12 @@ FalconStrategy::FalconStrategy(std::string name, Falcon &falcon, Controllers &co
 
 void FalconStrategy::OnUpdate(double dt) {
   double powerOut = _contGroup.Get(ControlMap::FalconOn) > ControlMap::XboxDeadzone ? _contGroup.Get(ControlMap::FalconOn) : 0;
-  _falcon.setPower(powerOut);
+  if (powerOut != 0) {
+  _falcon.setPower(0.2);
+  } else {
+    _falcon.setPower(0);
+  }
+
 
   if (_contGroup.Get(ControlMap::EncoderRead, wml::controllers::Controller::ONFALL)) {
     _falcon.encoder();
